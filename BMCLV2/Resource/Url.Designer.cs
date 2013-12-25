3986d5cacd7a10d4036a30c20dff2abc355816ee
bangbang93 <bangbang93@163.com>
2013-12-25 23:04:24 +0800

    修正作者源下载地址错误

--- BMCLV2/Resource/Url.Designer.cs
+++ BMCLV2/Resource/Url.Designer.cs
@@ -79,7 +79,7 @@ namespace BMCLV2.Resource {
         }
         
         /// <summary>
-        ///   查找类似 http://www.bangbang93.com/bmcl/download/ 的本地化字符串。
+        ///   查找类似 http://www.bangbang93.com/bmcl/download/libraries/ 的本地化字符串。
         /// </summary>
         internal static string URL_LIBRARIES_bangbang93 {
             get {
