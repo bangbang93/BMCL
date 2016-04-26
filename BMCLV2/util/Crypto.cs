2e483958e6555c6aac4977456e363477237d81cf
bangbang93 <bangbang93@163.com>
2016-04-26 12:14:55 +0800

    sha1 crypto

---
+++ BMCLV2/util/Crypto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace BMCLV2.util
+{
+    class Crypto
+    {
+        public static string GetMd5HashFromFile(string filename)
+        {
+            var file = new FileStream(filename, FileMode.Open);
+            var md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            var retVal = md5.ComputeHash(file);
+            file.Close();
+
+            var sb = new StringBuilder();
+            foreach (var t in retVal)
+            {
+                sb.Append(t.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+        public static string GetSha1HashFromFile(string filename)
+        {
+            var file = new FileStream(filename, FileMode.Open);
+            var sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider();
+            var retVal = sha1.ComputeHash(file);
+            file.Close();
+
+            var sb = new StringBuilder();
+            foreach (var t in retVal)
+            {
+                sb.Append(t.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
