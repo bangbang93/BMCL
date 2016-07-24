3177fd4f7e318f1c67719b9c7c5a6d463b94d67a
bangbang93 <bangbang93@163.com>
2016-07-24 22:36:20 +0800

    允许不选择最近使用的版本保存配置

--- BMCLV2/Windows/MainWindowTab/GridConfig.xaml.cs
+++ BMCLV2/Windows/MainWindowTab/GridConfig.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Input;
 using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using BMCLV2.Exceptions;
 using BMCLV2.I18N;
 using BMCLV2.Mirrors;
 
@@ -30,7 +31,14 @@ namespace BMCLV2.Windows.MainWindowTab
             BmclCore.Config.Javaw = txtJavaPath.Text;
             BmclCore.Config.Javaxmx = txtJavaXmx.Text;
             BmclCore.Config.Login = listAuth.SelectedItem.ToString();
-            BmclCore.Config.LastPlayVer = BmclCore.MainWindow.GridGame.GetSelectedVersion();
+            try
+            {
+                BmclCore.Config.LastPlayVer = BmclCore.MainWindow.GridGame.GetSelectedVersion();
+            }
+            catch (NoSelectGameException)
+            {
+                BmclCore.Config.LastPlayVer = null;
+            }
             BmclCore.Config.Passwd = Encoding.UTF8.GetBytes(txtPwd.Password);
             BmclCore.Config.Username = txtUserName.Text;
             BmclCore.Config.WindowTransparency = sliderWindowTransparency.Value;
