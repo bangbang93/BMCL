148ae02084800d09069cee12d98a203203f2bc11
bangbang93 <bangbang93@163.com>
2017-02-04 13:06:21 +0800

    修正官方源的Assets无法正常使用

--- BMCLV2/Mirrors/Vanilla/Asset.cs
+++ BMCLV2/Mirrors/Vanilla/Asset.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using BMCLV2.Game;
 using BMCLV2.JsonClass;
 using BMCLV2.util;
@@ -8,7 +10,7 @@ namespace BMCLV2.Mirrors.Vanilla
 {
     public class Asset : Interface.Asset
     {
-        private const string Server = "https://s3.amazonaws.com/Minecraft.Download/";
+        private const string Server = "http://resources.download.minecraft.net";
 
         public override async Task<AssetsIndex> GetAssetsIndex(VersionInfo versionInfo, string savePath)
         {
@@ -20,12 +22,18 @@ namespace BMCLV2.Mirrors.Vanilla
                 assetIndexString = await Downloader.DownloadStringTaskAsync(versionInfo.AssetIndex.Url);
             savePath = Path.Combine(savePath, $"{assetIndex?.Id ?? versionInfo.Assets}.json");
             FileHelper.WriteFile(savePath, assetIndexString);
-            return new JSON<AssetsIndex>().Parse(assetIndexString);
+            // assets的json比较奇葩，不能直接通过反序列化得到
+            var jsSerializer = new JavaScriptSerializer();
+            var assetsObject = jsSerializer.Deserialize<Dictionary<string, Dictionary<string, AssetsIndex.Assets>>>(assetIndexString);
+            return new AssetsIndex
+            {
+                Objects = assetsObject["objects"]
+            };
         }
 
         public override async Task GetAssetsObject(AssetsIndex.Assets assets, string savePath)
         {
-            var uri = $"{Server}objects/{savePath}";
+            var uri = $"{Server}/{assets.Path}";
             savePath = Path.Combine(savePath, assets.Path);
             await Downloader.DownloadFileTaskAsync(uri, savePath);
         }
