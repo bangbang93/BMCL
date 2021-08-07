8f77678b62c4a7cca9c7f9428fe920416c87a971
bangbang93 <bangbang93@163.com>
2021-08-08 01:39:53 +0800

    fix delete

--- BMCLV2/Mirrors/MCBBS/Asset.cs
+++ BMCLV2/Mirrors/MCBBS/Asset.cs
@@ -14,25 +14,6 @@ namespace BMCLV2.Mirrors.MCBBS
         private const string Server = "https://download.mcbbs.net/";
         private readonly Regex _originServer = new Regex(@"https://launchermeta\.mojang\.com/");
 
-        public override async Task<AssetsIndex> GetAssetsIndex(VersionInfo versionInfo, string savePath)
-        {
-            string assetIndexString;
-            var assetIndex = versionInfo.AssetIndex;
-            if (assetIndex == null)
-                assetIndexString = await Downloader.DownloadStringTaskAsync($"{Server}indexes/{versionInfo.Assets}.json");
-            else
-                assetIndexString = await Downloader.DownloadStringTaskAsync(_originServer.Replace(versionInfo.AssetIndex.Url, Server));
-            savePath = Path.Combine(savePath, $"{assetIndex?.Id ?? versionInfo.Assets}.json");
-            FileHelper.WriteFile(savePath, assetIndexString);
-            // assets的json比较奇葩，不能直接通过反序列化得到
-            var jsSerializer = new JavaScriptSerializer();
-            var assetsObject = jsSerializer.Deserialize<Dictionary<string, Dictionary<string, AssetsIndex.Assets>>>(assetIndexString);
-            return new AssetsIndex
-            {
-                Objects = assetsObject["objects"]
-            };
-        }
-
         public override async Task GetAssetsObject(AssetsIndex.Assets assets, string savePath)
         {
             var uri = $"{Server}objects/{assets.Path}".Replace('\\', '/');
