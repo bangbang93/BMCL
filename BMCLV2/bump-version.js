979a585489af6b0f0d08be82b04364a4df5afb17
bangbang93 <bangbang93@163.com>
2021-08-08 15:35:15 +0800

    bump version

---
+++ BMCLV2/bump-version.js
@@ -0,0 +1,9 @@
+﻿const fs = require('fs')
+
+const file = './Properties/AssemblyInfo.cs'
+
+const context = fs.readFileSync(file, 'utf8')
+fs.writeFileSync(file, context.replace(/\[assembly: AssemblyVersion\("\d+\.\d+\.\d+\.(\d+)"\)]/, (str, build) => {
+  const buildNumber = parseInt(build, 10)
+  return str.replace(build, (buildNumber + 1).toString())
+}))
